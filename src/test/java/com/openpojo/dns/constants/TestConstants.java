e664616913b979b323d73604a85853de8d5421fa
oshoukry <oshoukry@openpojo.com>
2018-02-28 18:22:02 -0800

    Setting server 2 to a single IPv4 server.

--- src/test/java/com/openpojo/dns/constants/TestConstants.java
+++ src/test/java/com/openpojo/dns/constants/TestConstants.java
@@ -39,8 +39,8 @@ public class TestConstants {
   public static final String SERVER_1_IPv6_STRING = "2001:503:ba3e::2:30";
   public static final byte[] SERVER_1_IPv6_BYTES = getAddressBytes(SERVER_1_IPv6_STRING);
 
-  public static final String SERVER_2_NAME = "localhost.";
-  public static final String SERVER_2_IPv4_STRING = "127.0.0.1";
+  public static final String SERVER_2_NAME = "egyptianmuseum.gov.eg.";
+  public static final String SERVER_2_IPv4_STRING = "163.121.19.186";
   public static final byte[] SERVER_2_IPv4_BYTES = getAddressBytes(SERVER_2_IPv4_STRING);
 
   private static byte[] getAddressBytes(String ip_asString) {
