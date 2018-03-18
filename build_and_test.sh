4c7151391872ae5ee807062c482ffc6a8d60fc10
oshoukry <oshoukry@openpojo.com>
2018-03-18 16:06:47 -0700

    Adding utility script to facilitate testing on multiple vms and aggregate build coverage reports.

---
+++ build_and_test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This function on MAC allows you to easily switch which JVM to use on the command line.
+
+function switch_jdk() {
+  local wanted_java_version=$1
+  export JAVA_HOME=`/usr/libexec/java_home -F -v $wanted_java_version -a x86_64 -d64`
+
+  # cleaned PATH
+  export PATH=$(echo $PATH | sed -E "s,(/System)?/Library/Java/JavaVirtualMachines/[a-zA-Z0-9._]+/Contents/Home/bin:,,g")
+
+  # prepend wanted JAVA_HOME
+  export PATH=$JAVA_HOME/bin:$PATH
+
+  echo "Now using : "
+  java -version
+}
+
+switch_jdk 1.7
+mvn clean test jacoco:report
+switch_jdk 1.8
+mvn compile test jacoco:prepare-agent jacoco:report
+switch_jdk 9
+mvn test jacoco:report
