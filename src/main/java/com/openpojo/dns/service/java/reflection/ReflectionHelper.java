c19488c3f9b315fe8b358fa96917312e236f70b9
oshoukry <oshoukry@openpojo.com>
2018-03-18 15:47:35 -0700

    Adding java reflection facility to provide the support necessary to resolve issue #2

---
+++ src/main/java/com/openpojo/dns/service/java/reflection/ReflectionHelper.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018-2018 Osman Shoukry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.openpojo.dns.service.java.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author oshoukry
+ */
+public class ReflectionHelper {
+
+  public static Class<?> loadClass(String className) throws ClassNotFoundException {
+    try {
+      return Class.forName(className);
+    } catch (Throwable t) {
+      throw new ClassNotFoundException("failed to load class [" + className + "]", t);
+    }
+  }
+
+  @SuppressWarnings("WeakerAccess")
+  public static Field getFieldByName(Class<?> clazz, String fieldName) {
+    try {
+      final Field declaredField = clazz.getDeclaredField(fieldName);
+      declaredField.setAccessible(true);
+      return declaredField;
+    } catch (NoSuchFieldException ignored) { }
+    return null;
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  public static <T> boolean setFieldValue(Class<T> clazz, String fieldName, T instance, Object fieldValue) {
+    Field field = getFieldByName(clazz, fieldName);
+    try {
+      field.set(instance, fieldValue);
+      return true;
+    } catch (Exception ignored) { }
+    return false;
+  }
+
+  public static Object createProxy(ClassLoader classLoader, InvocationHandler invocationHandler, Class ... interfaces) {
+    return Proxy.newProxyInstance(classLoader, interfaces, invocationHandler);
+  }
+
+  private ReflectionHelper() {}
+}
---
+++ src/test/java/com/openpojo/dns/service/java/reflection/ReflectionHelperTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2018-2018 Osman Shoukry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.openpojo.dns.service.java.reflection;
+
+import com.openpojo.random.RandomFactory;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static com.openpojo.dns.service.java.reflection.ReflectionHelper.getFieldByName;
+import static com.openpojo.dns.service.java.reflection.ReflectionHelper.loadClass;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author oshoukry
+ */
+public class ReflectionHelperTest {
+  private String someFieldToLoad;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void shouldThrowUnknownClassException() throws ClassNotFoundException {
+    final String someNonExistentClassName = "SomeNonExistentClass";
+    thrown.expect(ClassNotFoundException.class);
+    thrown.expectMessage(someNonExistentClassName);
+
+    loadClass(someNonExistentClassName);
+  }
+
+  @Test
+  public void canLoadExistingClass() throws ClassNotFoundException {
+    assertThat(loadClass(this.getClass().getName()), Matchers.<Class<?>>is(this.getClass()));
+  }
+
+  @Test
+  public void shouldReturnNullIfFieldNotPresent() {
+    assertThat(getFieldByName(this.getClass(), "SomeNonExistentField"), nullValue());
+  }
+
+  @Test
+  public void shouldReturnFieldIfPresent() {
+    assertThat(getFieldByName(this.getClass(), "someFieldToLoad"), notNullValue());
+  }
+
+  @Test
+  public void shouldReturnFalseIFFailedToSetValue() {
+    assertThat(someFieldToLoad, nullValue());
+    assertThat(ReflectionHelper.setFieldValue(ReflectionHelperTest.class, "someFieldToLoad", this, new Object()), is(false));
+    assertThat(someFieldToLoad, nullValue());
+  }
+
+  @Test
+  public void shouldSetFieldValueAndReturnTrue() {
+    String anyString = RandomFactory.getRandomValue(String.class);
+    assertThat(someFieldToLoad, nullValue());
+    assertThat(ReflectionHelper.setFieldValue(ReflectionHelperTest.class, "someFieldToLoad", this, anyString), is(true));
+    assertThat(someFieldToLoad, is(anyString));
+
+  }
+
+}
